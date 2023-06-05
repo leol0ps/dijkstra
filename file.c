e8dbfe2e0a4433b7defc529282f21ce2b76d059f
leol0ps <leotimao2@hotmail.com>
2023-06-05 00:53:02 -0300

    solved reading problem

--- file.c
+++ file.c
@@ -11,8 +11,9 @@ double** read_entry(char* entry_name, int* o,int* d,int* v){
 	char* aux = NULL;
 	int n_vertices;
 	int n_edges;
-	int edge_s,edge_d;
+	int edge_s,edge_d = 0;
 	double v_start;
+	double value;
 	entry = fopen(entry_name,"r");
 	if(entry == NULL){
 		printf("sem arquivo \n");
@@ -37,29 +38,33 @@ double** read_entry(char* entry_name, int* o,int* d,int* v){
 	read = getline(&line,&len,entry);
 	v_start = atof(line);
 	double** vet_edges = malloc(n_vertices*sizeof(double*));
-	for(int i = 0 ; i < n_edges; i++){
+	for(int i = 0 ; i < n_vertices; i++){
 		vet_edges[i] = malloc(n_vertices*sizeof(double));
 	}
 	for(int i = 0; i < n_vertices; i++){
 		for(int j = 0; j < n_vertices; j++)
 			vet_edges[i][j] = 0;
 	}
-	printf("%d %d %lf\n", origem,destino,v_start);
+	printf("%d %d %lf %d\n", origem,destino,v_start,n_edges);
 	for(int i = 0; i < n_edges ; i++){
 		read = getline(&line,&len,entry);
 		aux = strtok(line,";");
 		edge_s = atoi(aux);
 		int j = 0;
 		while(aux != NULL){
-			if(j==0){
+			if(j==1){
 				edge_d = atoi(aux);
 			}
-			else{
-				vet_edges[edge_s-1][edge_d-1] = atof(aux);
+			else if(j==2){
+				value = atof(aux);
 			}
 			j++;
 			aux = strtok(NULL,";");
+		
 		}
+		
+		printf("creating edge with %d %d position\n",edge_s,edge_d);
+		vet_edges[edge_s-1][edge_d-1] = value;
 	}
 	*o = origem;
     *v = n_vertices;	
--- main.c
+++ main.c
@@ -5,11 +5,15 @@ int main(int argc, char** argv){
 	int origem;
 	int destino;
 	int v;
+	int count = 0;
 	double** vet_edges = read_entry(argv[1],&origem,&destino,&v);
 	for(int i = 0; i < v; i++){
 		for(int k = 0; k < v; k++){
 			printf(" arestas %d %d tem peso %lf\n",i,k,vet_edges[i][k]);
+		if(vet_edges[i][k]> 0)
+				count++;
 		}
 	}
+	printf("count %d \n",count);
 	return 0;
 }
