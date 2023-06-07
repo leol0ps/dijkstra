383ae7f87ac3f8a187d656c9de47f8b1584d8686
leol0ps <leotimao2@hotmail.com>
2023-06-07 11:51:13 -0300

    fixed print formart for vertices

--- main.c
+++ main.c
@@ -23,26 +23,18 @@ int main(int argc, char** argv){
 	}
 	printf("%d %d \n", origem ,  destino);
 	int* result = rota(vet_edges,v,(origem-1),(destino-1),att,&time,&path_size);
-	for(int i = 0; i < path_size; i++){
-		printf("%d  \n", result[i]+1);
+	for(int i = 0; i < path_size-1; i++){
+		printf("%d;", result[i]+1);
 	}
+	printf("%d\n",result[path_size-1]+1);
 	//print_list(att);
 	//free_list(att);
 	double distance = 0;
 	for(int i = 1; i < path_size; i++){
 		distance+= edge_distance(vet_edges[result[i-1]][result[i]]);
 		
-		printf("%d %d edge\n", result[i-1],result[i]);
+	//	printf("%d %d edge\n", result[i-1],result[i]);
 	}
-	int k = path_size;
-	printf("vale k %d\n",k);
-	int x = k-1;
-	/*while(x > 0){ 
-		distance += edge_distance(vet_edges[result[x]][result[k]]);
-		k = x;
-		x = k-1;
-		printf("%d %d edge\n", result[x],result[k]);
-	}*/
 	free_mat_edge(vet_edges,v);
 	free(result);
 	printf("path time : %lf  e %lf \n", time,distance);
